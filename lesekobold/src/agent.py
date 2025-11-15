d4d7aaa0d5a5d57e5a810d42073e6d6090900c36
zweiss <zarah.weiss@gmx.de>
2025-11-15 10:36:51 +0100

    feat(llm): :construction: add first agent for story

--- lesekobold/src/agent.py
+++ lesekobold/src/agent.py
@@ -9,12 +9,33 @@ from config import llm_config
 logging.basicConfig(level=logging.ERROR)
 
 
-class Agent(google.adk.Agent):
+class StoryAgent(google.adk.AgentTemplate):
+    """
+    Agent for story generation.
+    """
+
     def __init__(self, name: str):
         super().__init__(name)
 
-    def run(self):
-        print(f"Running {self.name}")
+    def build(self) -> google.adk.Agent:
+        # TODO figure out how to use the model from litellm
+        model = litellm.completion(llm_config.MODEL_NAME)
+
+        # TODO: move the prompts to a separate file and load them with jinja2
+        rval = google.adk.Agent(
+            name="story_agent_v1",
+            model=model,  # Can be a string for Gemini or a LiteLlm object
+            description="Generates stories based on a user's prompt.",
+            instruction="You are a helpful story generator for educational children's stories. "
+            "You create stories based on the content specified by the user. "
+            "You then adjust the language and style of the story to make it more engaging and interesting for children. Make sure that the reading level is appropriate for the specified age group."
+            "Write all stories in German.",
+            # tools=[get_story],  # Pass the function directly
+        )
+        logging.info(
+            f"Agent '{rval.name}' created using model '{llm_config.MODEL_NAME}'."
+        )
+        return rval
 
 
 def main():
